a03669949ce7af2cecdec7f7ff34440fbc647319
chantmk <chan_tham@hotmail.com>
2020-11-15 14:41:40 +0000

    create app

---
+++ Finding-imposter/x/Findingimposter/client/cli/tx.go
@@ -0,0 +1,29 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/Finding-imposter/x/Findingimposter/types"
+)
+
+// GetTxCmd returns the transaction commands for this module
+func GetTxCmd(cdc *codec.Codec) *cobra.Command {
+	FindingimposterTxCmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	FindingimposterTxCmd.AddCommand(flags.PostCommands(
+    // this line is used by starport scaffolding
+	)...)
+
+	return FindingimposterTxCmd
+}
