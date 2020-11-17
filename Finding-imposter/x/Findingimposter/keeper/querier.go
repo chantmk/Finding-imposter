e1227d1979d47aa5bb753456ae802cdcaa3cd23c
chantmk <chan_tham@hotmail.com>
2020-11-17 13:01:59 +0000

    May be fix pending covid bug

--- Finding-imposter/x/Findingimposter/keeper/querier.go
+++ Finding-imposter/x/Findingimposter/keeper/querier.go
@@ -30,7 +30,7 @@ func NewQuerier(k Keeper) sdk.Querier {
 		case types.QueryListDoctor:
 			return listDoctor(ctx, k)
 		case types.QueryListPendingCovid:
-			return listPendingCovid	(ctx, k)
+			return listPendingCovid(ctx, k)
 		default:
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, "unknown Findingimposter query endpoint")
 		}
