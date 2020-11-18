e8e3aa787c585ec0f0c27d677b6518cbc2a98a6a
chantmk <chan_tham@hotmail.com>
2020-11-18 06:55:36 +0000

    cli list with creator address covid, quarantine, log

--- Finding-imposter/x/Findingimposter/client/cli/queryQuarantine.go
+++ Finding-imposter/x/Findingimposter/client/cli/queryQuarantine.go
@@ -26,3 +26,28 @@ func GetCmdListQuarantine(queryRoute string, cdc *codec.Codec) *cobra.Command {
 		},
 	}
 }
+
+func GetCmdListSpecQuarantine(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:   "list-spec-quarantine [address] ...",
+		Short: "list specific address quarantine",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+			res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/"+types.QueryListQuarantine, queryRoute), nil)
+			if err != nil {
+				fmt.Printf("could not list specific Quarantine\n%s\n", err.Error())
+				return nil
+			}
+			var out []types.Quarantine
+			cdc.MustUnmarshalJSON(res, &out)
+
+			var filteredOut []types.Quarantine
+			for _, quarantine := range out {
+				if isin(quarantine.Creator.String(), args) {
+					filteredOut = append(filteredOut, quarantine)
+				}
+			}
+			return cliCtx.PrintOutput(filteredOut)
+		},
+	}
+}
