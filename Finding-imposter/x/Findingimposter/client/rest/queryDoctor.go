6c46fb94e6cb4055238434d0fcd25d141715e7ba
chantmk <chan_tham@hotmail.com>
2020-11-18 08:13:09 +0000

    rest list spec quarantune covid remove doctor

--- Finding-imposter/x/Findingimposter/client/rest/queryDoctor.go
+++ Finding-imposter/x/Findingimposter/client/rest/queryDoctor.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/types/rest"
-	"github.com/Finding-imposter/x/Findingimposter/types"
 )
 
 func listDoctorHandler(cliCtx context.CLIContext, storeName string) http.HandlerFunc {
@@ -17,43 +16,5 @@ func listDoctorHandler(cliCtx context.CLIContext, storeName string) http.Handler
 			return
 		}
 		rest.PostProcessResponse(w, cliCtx, res)
-	}
-}
-
-type listSpecDoctor struct {
-	BaseReq rest.BaseReq `json:"base_req"`
-	Address []string `json:address`
-}
-
-func listSpecDoctorHandler(cliCtx context.CLIContext, storeName string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/list-doctor", storeName), nil)
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
-			return
-		}
-
-		var req listSpecDoctor
-		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, "failed to parse request")
-			return
-		}
-		baseReq := req.BaseReq.Sanitize()
-		if !baseReq.ValidateBasic(w) {
-			return
-		}
-		address := req.Address
-
-		var out []types.Doctor
-		cliCtx.Codec.MustUnmarshalJSON(res, &out)
-
-		var filteredOut []types.Doctor
-			for _, doctor := range out {
-				if isin(doctor.Creator.String(),address) {
-					filteredOut = append(filteredOut, doctor)
-				}
-			}
-		// response = cliCtx.Codec.MusMarshalJSON(filteredOut)
-		rest.PostProcessResponse(w, cliCtx, filteredOut)
 	}
 }
--- Finding-imposter/x/Findingimposter/client/rest/rest.go
+++ Finding-imposter/x/Findingimposter/client/rest/rest.go
@@ -11,15 +11,16 @@ func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
   // this line is used by starport scaffolding
 	r.HandleFunc("/Findingimposter/quarantine", listQuarantineHandler(cliCtx, "Findingimposter")).Methods("GET")
 	r.HandleFunc("/Findingimposter/quarantine", createQuarantineHandler(cliCtx)).Methods("POST")
+	r.HandleFunc("/Findingimposter/quarantine", listSpecQuarantineHandler(cliCtx, "Findingimposter")).Methods("Post")
 	r.HandleFunc("/Findingimposter/covid", listCovidHandler(cliCtx, "Findingimposter")).Methods("GET")
-	r.HandleFunc("/Findingimposter/pendingCovid", listPendingCovidHandler(cliCtx, "Findingimposter")).Methods("GET")
 	r.HandleFunc("/Findingimposter/covid", createCovidHandler(cliCtx)).Methods("POST")
+	r.HandleFunc("/Findingimposter/covid/pending", listPendingCovidHandler(cliCtx, "Findingimposter")).Methods("GET")
+	r.HandleFunc("/Findingimposter/covid/list", listSpecCovidHandler(cliCtx, "Findingimposter")).Methods("Post")
 	r.HandleFunc("/Findingimposter/log", listLogHandler(cliCtx, "Findingimposter")).Methods("GET")
 	r.HandleFunc("/Findingimposter/log", createLogHandler(cliCtx)).Methods("POST")
 	r.HandleFunc("/Findingimposter/log/list", listSpecLogHandler(cliCtx, "Findingimposter")).Methods("Post")
 	r.HandleFunc("/Findingimposter/doctor", listDoctorHandler(cliCtx, "Findingimposter")).Methods("GET")
 	r.HandleFunc("/Findingimposter/doctor", createDoctorHandler(cliCtx)).Methods("POST")
-	r.HandleFunc("/Findingimposter/doctor/list", listSpecDoctorHandler(cliCtx, "Findingimposter")).Methods("Post")
 }
 
 func isin(address string, list []string) bool {
